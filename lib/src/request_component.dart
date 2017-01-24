a08ec9945e8b11ddf0bdbcf404967e36d4d5bc85
Dmitriy <Dmitriy.Litovchenko@yamalspg.ru>
2017-01-24 11:43:06 +0300

    -

---
+++ lib/src/request_component.dart
@@ -0,0 +1,43 @@
+import 'dart:js';
+import 'package:angular2/core.dart';
+import 'package:angular2/router.dart';
+
+import 'package:resources_loader/resources_loader.dart';
+
+@Component(
+    selector: 'request',
+    templateUrl: 'request_component.html')
+class RequestComponent implements AfterViewInit {
+  static const String route_name = 'Request';
+  static const String route_path = 'Request';
+  static const Route route = const Route(
+      path: RequestComponent.route_path,
+      component: RequestComponent,
+      name: RequestComponent.route_name);
+
+  final Router _router;
+  final ResourcesLoaderService _resourcesLoader;
+
+  RequestComponent(this._router, this._resourcesLoader) {}
+
+  @override
+  void ngAfterViewInit() {
+    _resourcesLoader.loadScript(
+      'assets/js/', 'app.js', false);
+
+    _resourcesLoader.loadScript(
+      'vendor/moment/min/', 'moment.min.js', false);
+
+    _resourcesLoader.loadScript(
+      'vendor/bootstrap-daterangepicker/', 'daterangepicker.js', false,
+      onData: () {
+        var object = new JsObject(context['\$'], ['#request-date-range-picker']);
+        object.callMethod('daterangepicker', null);
+      });
+
+    _resourcesLoader.loadStyle('vendor/bootstrap-daterangepicker/', 'daterangepicker.css');
+
+    _resourcesLoader.loadScript(
+      'vendor/bootstrap-daterangepicker/', 'daterangepicker.js', false);
+  }
+}
---
+++ web/main.dart
@@ -0,0 +1,33 @@
+import 'dart:core';
+
+import 'package:angular2/platform/browser.dart';
+import 'package:angular2/core.dart';
+import 'package:angular2/src/core/reflection/reflection.dart';
+import 'package:angular2/router.dart';
+import 'package:angular2/src/platform/browser/location/hash_location_strategy.dart';
+import 'package:angular2/platform/common.dart';
+
+import 'package:resources_loader/resources_loader.dart';
+
+import 'package:request/request_component.dart';
+
+bool get isDebug =>
+    (const String.fromEnvironment('PRODUCTION', defaultValue: 'false')) != 'true';
+
+main() async {
+  if (isDebug) {
+    reflector.trackUsage();
+  }
+
+  ComponentRef ref = await bootstrap(RequestComponent, [
+    ROUTER_PROVIDERS,
+    const Provider(ResourcesLoaderService),
+    const Provider(LocationStrategy, useClass: HashLocationStrategy)
+  ]);
+
+  if (isDebug) {
+    print('Application in DebugMode');
+    enableDebugTools(ref);
+    print('Unused keys: ${reflector.listUnusedKeys()}');
+  }
+}
