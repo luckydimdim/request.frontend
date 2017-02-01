37e4c65a6ba6d70bc419c4a8aca5f986d47246b6
Dmitriy <Dmitriy.Litovchenko@yamalspg.ru>
2017-02-01 14:59:30 +0300

    total cell calculation (cc-2)

--- lib/src/cc2_object_contractor/request_cc2_object_contractor_component.dart
+++ lib/src/cc2_object_contractor/request_cc2_object_contractor_component.dart
@@ -44,7 +44,26 @@ class RequestCc2ObjectContractorComponent implements AfterViewInit, OnInit {
     _resourcesLoaderService.loadStyle(
         'vendor/bootstrap-daterangepicker/', 'daterangepicker.css');
 
-    _resourcesLoaderService.loadScript(
-        'packages/request/src/settings/', 'init-date-range.js', false);
+    var printButtonList = querySelectorAll('[print-cc2]') as List<ButtonElement>;
+    printButtonList.forEach((ButtonElement button) {
+      button.onClick.listen((MouseEvent e) {
+        _router.parent.parent.navigateByUrl('/cc2-print');
+      });
+    });
+
+    var table = querySelector('[calculation-table]') as TableElement;
+    table.rows.forEach((TableRowElement row) {
+      var priceCell = row.querySelector('[data-unit-price]') as TableCellElement;
+      var quantityInput = row.querySelector('[data-unit-quantity]') as InputElement;
+      var totalCell = row.querySelector('[data-total]') as TableCellElement;
+
+      if (totalCell != null) {
+        quantityInput.onChange.listen((Event e) {
+          var newQuantity = e.currentTarget as InputElement;
+
+          totalCell.innerHtml = (int.parse(priceCell.innerHtml, onError:(_) => 0) * int.parse(newQuantity.value, onError: (_) => 0)).toString() +' р.';
+        });
+      }
+    });
   }
 }
